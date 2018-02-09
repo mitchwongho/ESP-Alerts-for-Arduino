af1cfdb0600c80ec9ec7bb6e07b00321cec07ee5
mitchwongho <oreomitch@gmail.com>
2018-02-09 13:59:05 +0200

    Add new charactertistic to display time and battery status

--- sketches/esp32_android/esp32_android.ino
+++ sketches/esp32_android/esp32_android.ino
@@ -27,6 +27,7 @@
 #include <BLEUtils.h>
 #include <BLE2902.h>
 #include "espressif_logo.h"
+#include "battery_icon.h"
 //#include <BLEAdvertisedDevice.h>
 // Include the correct display library
 // For a connection via I2C using Wire include
@@ -38,16 +39,20 @@
 static const BLEUUID ALERT_DISPLAY_SERVICE_UUID = BLEUUID("3db02924-b2a6-4d47-be1f-0f90ad62a048");
 static const BLEUUID DISPLAY_MESSAGE_CHARACTERISTIC_UUID = BLEUUID("8d8218b6-97bc-4527-a8db-13094ac06b1d");
 static const BLEUUID DISPLAY_TIME_CHARACTERISTIC_UUID = BLEUUID("b7b0a14b-3e94-488f-b262-5d584a1ef9e1");
+static const BLEUUID DISPLAY_DISPLAY_ORIENTATION_CHARACTERISTIC_UUID = BLEUUID("0070b87e-d825-43f5-be0c-7d86f75e4900");
 
 #define BUFF_LEN 256
 
 
 volatile bool connected = false; //0=advertising; 1=connected
-volatile bool hasText = false;
+volatile bool hasText = false, hasBattText = false;
 volatile bool hasTimeText = false;
 volatile bool hasLongText = false;
 volatile int yScrollPos = 0;
 char* payload = new char[BUFF_LEN];
+char* time_buffer = new char[BUFF_LEN];
+volatile int batt_level = 0; // 
+volatile int display_orientation = 0; //0=none;1=flip
 
 #define SCROLL_DELAY 6
 #define MAX_SCROLL_WINDOW_HEIGHT 75
@@ -55,6 +60,39 @@ char* payload = new char[BUFF_LEN];
 // Initialize the OLED display using Wire library
 SSD1306  display(0x3c, 5, 4);
 
+
+class DisplayOrientationCharacteristicCallback: public BLECharacteristicCallbacks {
+  void onWrite(BLECharacteristic* pCharacteristic) {
+    std::string str = pCharacteristic->getValue(); //read values
+    display_orientation = (int)str[0];
+    Serial.println(" DisplayOrientationCharacteristicCallback->onWrite()");
+  }
+  
+};
+/**
+ * Callback to handle Time Charcterisitc events
+ */
+class TimeCharacteristicCallback: public BLECharacteristicCallbacks {
+  void onWrite(BLECharacteristic* pTimeCharacteristic) {
+    std::string str = pTimeCharacteristic->getValue(); //read values
+    memset(time_buffer, '\0', sizeof(time_buffer)); //clear buffer
+    strcpy(time_buffer, &str.c_str()[1]); //first char indicates battery-level
+    batt_level = (int)str[0]; //char to int
+
+    
+    char buff[16];
+    sprintf(buff, "Batt: %d%%", str[0]);
+    Serial.print(buff);
+    Serial.println(" TimeCharacteristicCallback->onWrite()");
+
+    hasLongText = false;
+    hasText = false;
+    hasTimeText = true;
+  }
+};
+/**
+ * Callback to handle Notification characteristic events
+ */
 class DisplayCharacteristicCallback: public BLECharacteristicCallbacks {
   void onWrite(BLECharacteristic* pCharacteristic) {
     // write text to OLED
@@ -65,25 +103,15 @@ class DisplayCharacteristicCallback: public BLECharacteristicCallbacks {
     Serial.print(String(uuid_s.c_str()));
     Serial.print(",len=");
     Serial.print(str.length());
-    Serial.print(",type=");
-    Serial.print(str[0]);
     Serial.println("}");
 
     memset(payload, '\0', sizeof(payload));
-    strcpy(payload, &str.c_str()[1]); //first char indicates type
+    strcpy(payload, str.c_str());
     yScrollPos = 0;
 
-    char type = str[0];
-    
-    if (type == '0') {
-      hasLongText = str.length() > 40;
-      hasText = true;
-      hasTimeText = false;
-    } else if (type == '1') {
-      hasLongText = false;
-      hasText = false;
-      hasTimeText = true;
-    }
+    hasLongText = str.length() > 40;
+    hasText = true;
+    hasTimeText = false;
   }
 };
 
@@ -121,13 +149,19 @@ void setupBLEServer() {
 //                                          );
 //  pCharacteristic->setWriteProperty(true);
 
-  BLECharacteristic* pCharacteristicText = new BLECharacteristic(DISPLAY_MESSAGE_CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_WRITE);
+  BLECharacteristic* pCharacteristicText = new BLECharacteristic(DISPLAY_MESSAGE_CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_WRITE_NR); //Request MTU=500 from client
   pCharacteristicText->setCallbacks(new DisplayCharacteristicCallback());
   pService->addCharacteristic(pCharacteristicText);
 
-  // There seems to be an issue whe you have more than 1 WRITE characteristic
-//  BLECharacteristic* pCharacteristicTime = new BLECharacteristic(DISPLAY_TIME_CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_NOTIFY);
-//  pService->addCharacteristic(pCharacteristicTime); 
+  // Display Time Characteristic
+  BLECharacteristic* pCharacteristicTime = new BLECharacteristic(DISPLAY_TIME_CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_WRITE_NR);
+  pCharacteristicTime->setCallbacks(new TimeCharacteristicCallback());
+  pService->addCharacteristic(pCharacteristicTime); 
+
+  // Change Display Orientation
+  BLECharacteristic* pCharacteristicDisplayOrientation = new BLECharacteristic(DISPLAY_DISPLAY_ORIENTATION_CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_WRITE_NR);
+  pCharacteristicDisplayOrientation->setCallbacks(new DisplayOrientationCharacteristicCallback());
+  pService->addCharacteristic(pCharacteristicDisplayOrientation); 
 
   pService->start();
      
@@ -149,7 +183,7 @@ void setup() {
   // Initialising the UI will init the display too.
   display.init();
 
-  display.flipScreenVertically();
+//  display.flipScreenVertically();
   display.setFont(ArialMT_Plain_10);
 }
 
@@ -172,10 +206,28 @@ void oledWriteText(char* text, uint32_t y = 0) {
   display.drawStringMaxWidth(0, y, 120, text);
 }
 
-void oledWriteTimeText(char* text) {
+void drawBatteryIcon() {
+    // see http://blog.squix.org/2015/05/esp8266-nodemcu-how-to-create-xbm.html
+    // on how to create xbm files
+    display.drawXbm(0, 49, batt_icon_width, batt_icon_height, batt_icon_bits);
+}
+
+void oledWriteBattLevel(int batt_level) {
+  char buff[16];
+//  itoa(batt_level, buff, 10);
+  drawBatteryIcon();
+  sprintf(buff, "%d%%", batt_level);
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.setFont(ArialMT_Plain_16);
+  display.drawString(15, 48, buff);
+}
+
+void oledWriteTimeText(char* text, int batt_level) {
   display.setTextAlignment(TEXT_ALIGN_LEFT);
   display.setFont(ArialMT_Plain_24);
-  display.drawString(36, 18, text);
+  display.drawString(36, 10, text);
+  //
+  oledWriteBattLevel(batt_level);
 }
 
 void drawImageDemo() {
@@ -200,8 +252,11 @@ void loop() {
       }
       oledWriteText(payload, yPos);
     } else if (hasTimeText) {
-      oledWriteTimeText(payload);
-    } else {  
+      oledWriteTimeText(time_buffer, batt_level);
+    } else if (display_orientation != 0) {
+      display.flipScreenVertically();
+      display_orientation = 0;
+    } else {
       oledWriteText("Connected");
     }
   } else {
@@ -214,7 +269,7 @@ void loop() {
   // write the buffer to the display
   display.display();
   //
-  delay(200); //250ms
+  delay(500); //500ms
 }
 
 
